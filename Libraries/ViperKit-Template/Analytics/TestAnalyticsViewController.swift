787222423192341248943cf12113ed0dc739e728
Josh Campion <joshcampion89@gmail.com>
2016-04-04 10:03:28 +0100

    Update frameworks for Swift 2.2 & Xcode 7.3

---
+++ Libraries/ViperKit-Template/Analytics/TestAnalyticsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  TestAnalyticsViewController.swift
+//  MozQuito
+//
+//  Created by Josh Campion on 22/01/2016.
+//  Copyright © 2016 The Distance. All rights reserved.
+//
+
+import UIKit
+
+import ViperKit
+
+class TestAnalyticsViewController: UIViewController, AnalyticScreenView {
+
+     var screenName:AnalyticScreen = .Test
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        registerScreenView()
+    }
+    
+    @IBAction func crashTapped() {
+        AppDependencies.sharedInstance().crashReportingInteractor?.simulateCrash()
+    }
+
+    @IBAction func eventTapped() {
+        let testEvent = AnalyticEvent(category: .Test, action: .Test, label: nil)
+        AppDependencies.sharedInstance().analyticsInteractor?.sendAnalytic(testEvent)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
