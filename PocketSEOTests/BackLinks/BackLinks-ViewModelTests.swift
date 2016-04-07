0e38b17b0cf4d3103ab064fb45da91623a3c0773
Josh Campion <joshcampion89@gmail.com>
2016-04-07 10:28:29 +0100

    Updating Tests to check for no errors

--- PocketSEOTests/BackLinks/BackLinks-ViewModelTests.swift
+++ PocketSEOTests/BackLinks/BackLinks-ViewModelTests.swift
@@ -14,13 +14,50 @@ import Nimble
 
 @testable import PocketSEO
 
+extension XCTestCase {
+    
+    /**
+     
+     Creates an `XCTExpectation` which fulfils after a given time interval and waits for it.
+     
+     - parameter time: The time to wait for. Default value is 0.25.
+     - parameter withDescription: The description for the `XCTExpectation`.
+     
+    */
+    func waitForTime(time:NSTimeInterval = 0.25, withDescription description:String) {
+        
+        // wait to see if there are no events sent after a possible refresh
+        let waitExpectation = expectationWithDescription(description)
+        waitExpectation.performSelector(#selector(XCTestExpectation.fulfill), withObject: nil, afterDelay: time)
+        waitForExpectationsWithTimeout(time + 0.15, handler: nil)
+        
+    }
+    
+}
+
 class BackLinks_ViewModelTests: XCTestCase {
 
+    var errors = MutableProperty<NSError?>(nil)
+    var found = MutableProperty<LinksOutput?>(nil)
+    
     let viewModel = LinksViewModel(apiManager: APIManager(urlStore: TestURLStore()))
     
     override func setUp() {
         super.setUp()
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        errors = MutableProperty<NSError?>(nil)
+        
+        errors <~ viewModel.errorSignal
+            .observeOn(UIScheduler())
+            .map({ $0 as NSError? })
+        
+        found = MutableProperty<LinksOutput?>(nil)
+        
+        found <~ viewModel.contentChangesSignal
+            .observeOn(UIScheduler())
+            .map({ $0 as LinksOutput? })
+
     }
     
     override func tearDown() {
@@ -31,44 +68,24 @@ class BackLinks_ViewModelTests: XCTestCase {
     // test no content is loaded on willAppear
     func testEmptyWillAppear() {
         
-        let found = MutableProperty<LinksOutput?>(nil)
-        let errors = MutableProperty<NSError?>(nil)
+        waitForTime(withDescription: #function)
         
-        found <~ viewModel.contentChangesSignal
-            .observeOn(UIScheduler())
-            .map({ $0 as LinksOutput? })
-        
-        errors <~ viewModel.errorSignal
-            .observeOn(UIScheduler())
-            .map({ $0 as NSError? })
-        
-        // Note this test doesn't validate this...
-        expect(found.value).toEventually(beNil())
-        expect(errors.value).toEventually(beNil())
+        expect(self.found.value).to(beNil())
+        expect(self.errors.value).to(beNil())
     }
     
     func testInitialPage() {
         
-        let found = MutableProperty<LinksOutput?>(nil)
-        let errors = MutableProperty<NSError?>(nil)
-        
-        found <~ viewModel.contentChangesSignal
-            .observeOn(UIScheduler())
-            .map({ $0 as LinksOutput? })
-        
-        errors <~ viewModel.errorSignal
-            .observeOn(UIScheduler())
-            .map({ $0 as NSError? })
-
         let expected = BackLink.distanceBackLink()
         
         viewModel.refreshObserver.sendNext((urlRequest: "thedistance.co.uk", nextPage: true))
 
-        expect(found.value?.links.count).toEventually(equal(25))
-        expect(found.value?.links[5]).toEventually(contentEqual(expected))
-        expect(found.value?.moreAvailable).toEventually(beTrue())
+        expect(self.found.value?.links.count).toEventually(equal(25))
+        expect(self.found.value?.links[5]).toEventually(contentEqual(expected))
+        expect(self.found.value?.moreAvailable).toEventually(beTrue())
         
-        expect(errors.value).toEventuallyNot(beTruthy())
+        waitForTime(withDescription: #function)
+        expect(self.errors.value).to(beNil())
     }
     
     func testNextPageLoad() {
@@ -84,9 +101,11 @@ class BackLinks_ViewModelTests: XCTestCase {
             .observeOn(UIScheduler())
             .map({ $0 as NSError? })
         
+        errors.producer.startWithNext { (error) in
+            XCTFail("Errored getting page: \(error)")
+        }
         
-        
-        let foundExpectation = expectationWithDescription(#function)
+        let foundExpectation = expectationWithDescription(#function + "Page")
         found.producer.startWithNext {
             if $0 != nil {
                 foundExpectation.fulfill()
@@ -110,6 +129,10 @@ class BackLinks_ViewModelTests: XCTestCase {
         expect(found.value?.links[5]).toEventually(contentEqual(expected1))
         expect(found.value?.links[26]).toEventually(contentEqual(expected2))
         expect(found.value?.moreAvailable).toEventually(beFalse())
+        
+        waitForTime(withDescription: #function + "Error")
+        
+        expect(self.errors.value).to(beNil())
     }
     
 }
